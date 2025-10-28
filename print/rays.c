f5c024863d85e98551d9862c7600cc08b32c947d
Tatiana Skubitskaya <tskubits@c2r3s4.42yerevan.am>
2025-10-28 17:01:50 +0400

    algo proccess

--- print/rays.c
+++ print/rays.c
@@ -25,28 +25,18 @@ t_wall	check_wall(t_game *game, t_point p, t_point dir)
 	return (wall);
 }
 
-t_point	calc_direction(double ray)
-{
-	t_point	dir;
-
-	dir.x = (cos(ray) > 0) - (cos(ray) < 0);
-	dir.y = (sin(ray) > 0) - (sin(ray) < 0);
-	return (dir);
-}
-
-t_wall	line_ray(t_game *game, double ray, t_point dir)
+t_wall	straight_line(t_game *game, double ray, t_point dir)
 {
 	int		point;
 	t_wall	wall;
 
+	(void)ray;
 	wall.x = game.player.x;
 	wall.y = game.player.y;
 	wall.color = new_color(-1, -1, -1);
 	if (dir.y == 0)
 	{
-		point = game.player.x / SCALE;
-		if (dir.x > 0)
-			point += 1;
+		point = game.player.x / SCALE + (dir.x > 0);
 		while (wall.color.r < 0 && point >= 0 && point < game.map_x)
 		{
 			wall = check_wall(game, new_point(point * SCALE, game.player.y), dir);
@@ -55,9 +45,7 @@ t_wall	line_ray(t_game *game, double ray, t_point dir)
 	}
 	else
 	{
-		point = game.player.y / SCALE;
-		if (dir.y > 0)
-			point += 1;
+		point = game.player.y / SCALE + (dir.y > 0);
 		while (wall.color.r < 0 && point >= 0 && point < game.map_y)
 		{
 			wall = check_wall(game, new_point(game.player.x, point * SCALE), dir);
@@ -67,9 +55,8 @@ t_wall	line_ray(t_game *game, double ray, t_point dir)
 	return (wall);
 }
 
-t_wall	find_wall(t_game *game, double ray)
+t_wall	inclined_line(t_game *game, double ray, t_point dir)
 {
-	t_point dir;
 	t_point x; // intersection with x-line
 	t_point	y; // intersection with y-line
 	t_wall	wall;
@@ -78,32 +65,38 @@ t_wall	find_wall(t_game *game, double ray)
 	wall.x = game.player.x;
 	wall.y = game.player.y;
 	wall.color = new_color(-1, -1, -1);
-	dir = calc_direction(ray);
-	if (dir.x == 0 || dir.y == 0)
-		return (line_ray(game, ray, dir));
-	line = new_point(game.player.x / SCALE, game.player.y / SCALE);
-	if (dir.x > 0)
-		line.x += 1;
-	if (dir.y > 0)
-		line.y += 1;
-	
+	line = new_point(game.player.x / SCALE + (dir.x > 0), \
+					game.player.y / SCALE + (dir.y > 0));
 	while (line.x >= 0 && line.y >= 0 && line.x < game.map_x && line.y < game.map_y)
 	{
 		x.x = line.x * SCALE;
 		y.y = line.y * SCALE;
 		x.y = game.player.y + (x.x - game.player.x) * tan(ray);
 		y.x = game.player.x + (y.y - game.player.y) / tan(ray);
-
 		if (abs(y.y - line.y) < abs(x.y - line.y))
+		{	
 			wall = check_wall(game, y);
-		else
-			wall = check_wall(game, x);
-		if (wall.color.r < 0)
-		{
-			line.x += dir.x;
 			line.y += dir.y;
 		}
-
+		else
+		{
+			wall = check_wall(game, x);
+			line.x += dir.x;
+		}
+		if (wall.color.r >= 0)
+			break ;
 	}
 	return (wall);
+}
+
+t_wall	find_wall(t_game *game, double ray)
+{
+	t_point dir;
+
+	dir.x = (cos(ray) > 0) - (cos(ray) < 0);
+	dir.y = (sin(ray) > 0) - (sin(ray) < 0);
+	if (dir.x == 0 || dir.y == 0)
+		return (straight_line(game, ray, dir));
+	else
+		return (inclined_line(game, ray, dir));
 }
