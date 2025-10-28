7c9004475edf163b051b131d5da2e2383c2b5704
Tatiana Skubitskaya <tskubits@c2r3s4.42yerevan.am>
2025-10-28 16:43:55 +0400

    algo proccess

--- print/rays.c
+++ print/rays.c
@@ -6,14 +6,14 @@ t_wall	check_wall(t_game *game, t_point p, t_point dir)
 
 	wall.x = p.x;
 	wall.y = p.y;
-	if (game->map[(p.y) / SCALE][(p.x + 5 * dir.x) / SCALE] == '1')
+	if (dir.x && game->map[(p.y) / SCALE][(p.x + 5 * dir.x) / SCALE] == '1')
 	{
 		if (dir.x > 0)
 			wall.color = new_color(195, 205, 250); // WEST
 		else
 			wall.color = new_color(195, 250, 205); // EAST
 	}
-	else if (game->map[(p.y + 5 * dir.y) / SCALE][(p.x) / SCALE] == '1')
+	else if (dir.y && game->map[(p.y + 5 * dir.y) / SCALE][(p.x) / SCALE] == '1')
 	{
 		if (dir.y > 0)
 			wall.color = new_color(250, 250, 190); // NORTH
@@ -29,15 +29,42 @@ t_point	calc_direction(double ray)
 {
 	t_point	dir;
 
-	if (cos(ray) > 0)
-		dir.x = 1;
-	else
-		dir.x = -1;
-	if (sin(ray) > 0)
-		dir.y = 1;
-	else
-		dir.y = -1;
+	dir.x = (cos(ray) > 0) - (cos(ray) < 0);
+	dir.y = (sin(ray) > 0) - (sin(ray) < 0);
 	return (dir);
+}
+
+t_wall	line_ray(t_game *game, double ray, t_point dir)
+{
+	int		point;
+	t_wall	wall;
+
+	wall.x = game.player.x;
+	wall.y = game.player.y;
+	wall.color = new_color(-1, -1, -1);
+	if (dir.y == 0)
+	{
+		point = game.player.x / SCALE;
+		if (dir.x > 0)
+			point += 1;
+		while (wall.color.r < 0 && point >= 0 && point < game.map_x)
+		{
+			wall = check_wall(game, new_point(point * SCALE, game.player.y), dir);
+			point += dir.x;
+		}
+	}
+	else
+	{
+		point = game.player.y / SCALE;
+		if (dir.y > 0)
+			point += 1;
+		while (wall.color.r < 0 && point >= 0 && point < game.map_y)
+		{
+			wall = check_wall(game, new_point(game.player.x, point * SCALE), dir);
+			point += dir.y;
+		}
+	}
+	return (wall);
 }
 
 t_wall	find_wall(t_game *game, double ray)
@@ -48,15 +75,12 @@ t_wall	find_wall(t_game *game, double ray)
 	t_wall	wall;
 	t_point line; // current point
 
-	if (sin(ray) == 0)
-		return (NULL);
-	if (cos(ray) == 0)
-		return (NULL);
-	
 	wall.x = game.player.x;
 	wall.y = game.player.y;
 	wall.color = new_color(-1, -1, -1);
 	dir = calc_direction(ray);
+	if (dir.x == 0 || dir.y == 0)
+		return (line_ray(game, ray, dir));
 	line = new_point(game.player.x / SCALE, game.player.y / SCALE);
 	if (dir.x > 0)
 		line.x += 1;
